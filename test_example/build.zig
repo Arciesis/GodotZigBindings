06e927da90c319a492bb1ff02976d419b5a335cb
Mathis Favier <favier.mathis@gmail.com>
2025-03-25 15:45:10 +0100

    added the binding_generator.zig file to the zon package

--- test_example/build.zig
+++ test_example/build.zig
@@ -4,24 +4,22 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardOptimizeOption(.{});
 
-    const lib = b.addSharedLibrary(
-        .{
-            .name = "gdextension_example",
-            .root_source_file = .{
-                .src_path = .{
-                    .owner = b,
-                    .sub_path = "src/lib.zig",
-                },
+    const lib = b.addSharedLibrary(.{
+        .name = "gdextension_example",
+        .root_source_file = .{
+            .src_path = .{
+                .owner = b,
+                .sub_path = "src/lib.zig",
             },
-            .version = .{
-                .major = 1,
-                .minor = 0,
-                .patch = 0,
-            },
-            .target = target,
-            .optimize = mode,
-        }
-    );
+        },
+        .version = .{
+            .major = 1,
+            .minor = 0,
+            .patch = 0,
+        },
+        .target = target,
+        .optimize = mode,
+    });
 
     const gdextension_package_path = "../src/lib.zig"; //On your own projects it would be wise to use an environment variable with a global path pointing to it
     lib.root_module.addImport(
